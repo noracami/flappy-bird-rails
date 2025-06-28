d70f28ed3ff6daba596e986f598523f0467d57f7
noracami <kerke.2011@gmail.com>
2025-06-28 21:47:13 +0800

    Add JavaScript

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@socketry/live", to: "@socketry--live.js" # @0.14.0
+pin "morphdom" # @2.7.5
